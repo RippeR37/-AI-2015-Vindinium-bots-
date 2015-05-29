6ab41275b423b6f55331d3f331d729d80c8b729f
RippeR <ripper37@gmail.com>
2015-05-29 21:39:06 +0200

    Fixed bug with simple getPath(state, start, end) method which prevented finding path.

--- Path.cpp
+++ Path.cpp
@@ -10,14 +10,16 @@ typedef Graph::GraphAdapter<State, Position, int> BaseGraphAdapter;
 /*** Simple A* graph adapter for getPath(state, start, end) method ***/
 class SimpleMapAdapter : public BaseGraphAdapter {
     public:
-        SimpleMapAdapter(const State& state) : BaseGraphAdapter(state) {
+        SimpleMapAdapter(const State& state, const Position& goal) : BaseGraphAdapter(state), _goal(goal) {
 
         }
 
         bool isAvailable(const Position& position) const {
             bool isAvailable = false;
 
-            if(_graph.get_tile_from_background_border_check(position) == Tile::EMPTY)
+            if(position == _goal)
+                isAvailable = true;
+            else if(_graph.get_tile_from_background_border_check(position) == Tile::EMPTY)
                 isAvailable = true;
 
             return isAvailable;
@@ -40,58 +42,62 @@ class SimpleMapAdapter : public BaseGraphAdapter {
             return dx + dy;
         }
 
+    private:
+        const Position& _goal;
+
 };
 
 /*** Simple A* graph adapter for getPath(state, start, end) method ***/
 class AdvancedMapAdapter : public BaseGraphAdapter {
-public:
-    AdvancedMapAdapter(const State& state) : BaseGraphAdapter(state) {
+    public:
+        AdvancedMapAdapter(const State& state) : BaseGraphAdapter(state) {
 
-    }
-
-    bool isAvailable(const Position& position) const {
-        bool isAvailable = false;
-
-        if(_graph.get_tile_from_background_border_check(position) == Tile::EMPTY)
-            isAvailable = true;
-
-        return isAvailable;
-    }
-
-    std::vector<NodeAdapterType> getNeighboursOf(const NodeAdapterType& node) const {
-        std::vector<NodeAdapterType> neighbours;
-
-        std::vector<std::pair<int, int>> posDiffs = {
-            {  0, -1 }, {  0,  1 }, { -1,  0 }, {  1,  0 }
-        };
-
-        for(const std::pair<int, int>& posDiff : posDiffs) {
-            neighbours.push_back(
-                NodeAdapterType(
-                    Position(
-                        node.position.x + posDiff.first,
-                        node.position.y + posDiff.second
-                    )
-                )
-            );
         }
 
-        return neighbours;
-    }
+        bool isAvailable(const Position& position) const {
+            bool isAvailable = false;
 
-    virtual int getHeuristicCostLeft(const NodeAdapterType& currentNode, const NodeAdapterType& goal) const {
-        // TODO: implement this
+            if(_graph.get_tile_from_background_border_check(position) == Tile::EMPTY)
+                isAvailable = true;
 
-        return 0;
-    }
+            // TODO: fix this - it should return true if above function returns tile from any of accepted type!
 
+            return isAvailable;
+        }
+
+        std::vector<NodeAdapterType> getNeighboursOf(const NodeAdapterType& node) const {
+            std::vector<NodeAdapterType> neighbours;
+
+            std::vector<std::pair<int, int>> posDiffs = {
+                {  0, -1 }, {  0,  1 }, { -1,  0 }, {  1,  0 }
+            };
+
+            for(const std::pair<int, int>& posDiff : posDiffs) {
+                neighbours.push_back(
+                    NodeAdapterType(
+                        Position(
+                            node.position.x + posDiff.first,
+                            node.position.y + posDiff.second
+                        )
+                    )
+                );
+            }
+
+            return neighbours;
+        }
+
+        int getHeuristicCostLeft(const NodeAdapterType& currentNode, const NodeAdapterType& goal) const {
+            // TODO: implement this
+
+            return 0;
+        }
 };
 
 
 Path::PathType Path::getPath(const State& state, const Position& start, const Position& end) {
     PathType result;
     Graph::AStar<State, Position, int> myAStar;
-    SimpleMapAdapter myMapAdapter(state);
+    SimpleMapAdapter myMapAdapter(state, end);
     SimpleMapAdapter::NodeAdapterType nodeStart(start);
     SimpleMapAdapter::NodeAdapterType nodeGoal(end);
 

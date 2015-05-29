a20847e9d295acb9f59ba6f375673fc9b44b17c3
RippeR <ripper37@gmail.com>
2015-05-29 19:20:01 +0200

    Added base graph pathfinding implementation.

---
+++ Path.h
@@ -0,0 +1,16 @@
+#ifndef PATH_H_INCLUDED
+#define PATH_H_INCLUDED
+
+#include "Graph/AStar.hpp"
+#include "tiles.h"
+#include "utlils.h"
+
+class Path : Graph::AStar<Tiles, int, double> {
+    public:
+        //
+
+    private:
+        //
+};
+
+#endif

561e5c57a1d0d571a0352d75717cddb053ea21a5
Mikołaj Nowak <ivan.legnica@gmail.com>
2015-05-30 21:21:49 +0200

    Heuristic cost in AdvancedMapAdapter

--- Path.cpp
+++ Path.cpp
@@ -2,10 +2,10 @@
 #include "Graph/AStar.hpp"
 
 #include <cmath>
+#include <climits>
 
 /*** Typedefs ***/
 typedef Graph::GraphAdapter<State, Position, int> BaseGraphAdapter;
-
 
 /*** Simple A* graph adapter for getPath(state, start, end) method ***/
 class SimpleMapAdapter : public BaseGraphAdapter {
@@ -107,15 +107,135 @@ class AdvancedMapAdapter : public BaseGraphAdapter {
         }
 
         int getHeuristicCostLeft(const NodeAdapterType& currentNode, const NodeAdapterType& currentNodeCpy) const {
-            // TODO: implement this
-            // Calculate closest possible distance to any node with type from _tileTypes;
-            // This would mean to find all objects with this type, store references to them and for each query
-            // check manhattan/euclidian distance to each of them and return smallest computed value
+            static std::vector<PositionType> tavernPositions = _getTavernPositions();
+            static std::vector<PositionType> minesPositions = _getMinesPositions();
 
-            return 0;
+            int result = INT_MAX;
+            PositionType position;
+            PositionType currentPosition = currentNode.position;
+            int distance, diffx, diffy;
+
+            for(Tile t: _tileTypes) {
+                switch(t) {
+                    case HERO1:
+                        position = _graph.heroes[0].position;
+                        diffx = currentPosition.x - position.x;
+                        diffy = currentPosition.y - position.y;
+                        distance = diffx * diffx + diffy * diffy;
+                        result = std::min(result, distance);
+                        break;
+                    case HERO2:
+                        position = _graph.heroes[1].position;
+                        diffx = currentPosition.x - position.x;
+                        diffy = currentPosition.y - position.y;
+                        distance = diffx * diffx + diffy * diffy;
+                        result = std::min(result, distance);
+                        break;
+                    case HERO3:
+                        position = _graph.heroes[2].position;
+                        diffx = currentPosition.x - position.x;
+                        diffy = currentPosition.y - position.y;
+                        distance = diffx * diffx + diffy * diffy;
+                        result = std::min(result, distance);
+                        break;
+                    case HERO4:
+                        position = _graph.heroes[3].position;
+                        diffx = currentPosition.x - position.x;
+                        diffy = currentPosition.y - position.y;
+                        distance = diffx * diffx + diffy * diffy;
+                        result = std::min(result, distance);
+                        break;
+                    case TAVERN:
+                        for(PositionType pos: tavernPositions) {
+                            diffx = currentPosition.x - pos.x;
+                            diffy = currentPosition.y - pos.y;
+                            distance = diffx * diffx + diffy * diffy;
+                            result = std::min(result, distance);
+                        }
+                        break;
+                    case MINE:
+                        for(PositionType pos: minesPositions) {
+                            bool owned = false;
+                            for(int i = 0; i < 4; ++i) {
+                                owned = (owned || (_graph.heroes[i].mine_positions.find(pos) != _graph.heroes[i].mine_positions.end()));
+                            }
+                            if(!owned) {
+                                diffx = currentPosition.x - pos.x;
+                                diffy = currentPosition.y - pos.y;
+                                distance = diffx * diffx + diffy * diffy;
+                                result = std::min(result, distance);
+                            }
+                        }
+                        break;
+                    case MINE1:
+                        for(PositionType pos: _graph.heroes[0].mine_positions) {
+                            diffx = currentPosition.x - pos.x;
+                            diffy = currentPosition.y - pos.y;
+                            distance = diffx * diffx + diffy * diffy;
+                            result = std::min(result, distance);
+                        }
+                        break;
+                    case MINE2:
+                        for(PositionType pos: _graph.heroes[1].mine_positions) {
+                            diffx = currentPosition.x - pos.x;
+                            diffy = currentPosition.y - pos.y;
+                            distance = diffx * diffx + diffy * diffy;
+                            result = std::min(result, distance);
+                        }
+                        break;
+                    case MINE3:
+                        for(PositionType pos: _graph.heroes[2].mine_positions) {
+                            diffx = currentPosition.x - pos.x;
+                            diffy = currentPosition.y - pos.y;
+                            distance = diffx * diffx + diffy * diffy;
+                            result = std::min(result, distance);
+                        }
+                        break;
+                    case MINE4:
+                        for(PositionType pos: _graph.heroes[3].mine_positions) {
+                            diffx = currentPosition.x - pos.x;
+                            diffy = currentPosition.y - pos.y;
+                            distance = diffx * diffx + diffy * diffy;
+                            result = std::min(result, distance);
+                        }
+                        break;
+                    default: break;
+                }
+            }
+
+            return ( (result == INT_MAX) ? 0 : result );
         }
 
     private:
+        std::vector<PositionType> _getTavernPositions() const {
+            int mapSize = _graph.get_tiles_full().shape()[0];
+            std::vector<PositionType> positions;
+            for(int i = 0; i < mapSize; ++i) {
+                for(int j = 0; j < mapSize; ++j) {
+                    PositionType position(i, j);
+                    if(_graph.get_tile_from_background(position) == TAVERN) {
+                        positions.push_back(position);
+                    }
+                }
+            }
+            return positions;
+        }
+
+        std::vector<PositionType> _getMinesPositions() const {
+            int mapSize = _graph.get_tiles_full().shape()[0];
+            std::vector<PositionType> positions;
+            for(int i = 0; i < mapSize; ++i) {
+                for(int j = 0; j < mapSize; ++j) {
+                    PositionType position(i, j);
+                    Tile t = _graph.get_tile_from_background(position);
+                    if(t == MINE || t == MINE1 || t == MINE2 || t == MINE3 || t == MINE4) {
+                        positions.push_back(position);
+                    }
+                }
+            }
+            return positions;
+        }
+
         const std::vector<Tile>& _tileTypes;
 };
 
